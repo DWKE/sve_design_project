8c3912d7680a35681177fbcb3ac9bc5f2abc39ba
soyeongkim <kimchuorok@gmailc.om>
2019-11-27 14:25:10 +0900

    outlier filter added

--- src/lidar_detection/ydlidar/src/scan_to_pcl.cpp
+++ src/lidar_detection/ydlidar/src/scan_to_pcl.cpp
@@ -17,6 +17,7 @@
 #include <tf/transform_broadcaster.h>
 #include "laser_geometry/laser_geometry.h"
 #include <pcl/filters/voxel_grid.h>
+#include <pcl/filters/radius_outlier_removal.h>
 
 
 using namespace std;
@@ -25,10 +26,14 @@ using namespace std;
 ros::Publisher pcl_from_scan;
 ros::Publisher filter_points;
 
+double m_fLeafSize;
+double m_fRadius;
+
 typedef pcl::PointCloud<pcl::PointXYZ> PointCloud;
 laser_geometry::LaserProjection projector;
 
-void downsample(const PointCloud::ConstPtr& pInputCloud, PointCloud::Ptr& pDownsampledCloud, float f_paramLeafSize_m);
+void downsample(const PointCloud::ConstPtr& pInputCloud, PointCloud::Ptr& pDownsampledCloud,
+                float f_paramLeafSize_m, float f_radius_size);
 
 void ydlidar_callback(const sensor_msgs::LaserScan::ConstPtr& scan_in)
 {
@@ -44,11 +49,11 @@ void ydlidar_callback(const sensor_msgs::LaserScan::ConstPtr& scan_in)
     pcl::fromROSMsg(cloud, *pInputCloud);
 
     PointCloud::Ptr pDownsampledCloud (new PointCloud);
-    downsample(pInputCloud, pDownsampledCloud, 0.1);
+    downsample(pInputCloud, pDownsampledCloud, m_fLeafSize, m_fRadius);
     filter_points.publish(pDownsampledCloud);
 }
 
-void downsample(const PointCloud::ConstPtr& pInputCloud, PointCloud::Ptr& pDownsampledCloud, float f_paramLeafSize_m)
+void downsample(const PointCloud::ConstPtr& pInputCloud, PointCloud::Ptr& pDownsampledCloud, float f_paramLeafSize_m, float f_radius_size)
 {
         pDownsampledCloud->clear();
 
@@ -57,12 +62,23 @@ void downsample(const PointCloud::ConstPtr& pInputCloud, PointCloud::Ptr& pDowns
         voxelFilter.setInputCloud (pInputCloud);
         voxelFilter.setLeafSize(f_paramLeafSize_m, f_paramLeafSize_m, f_paramLeafSize_m);
         voxelFilter.filter (*pDownsampledCloud);
+
+        pcl::RadiusOutlierRemoval<pcl::PointXYZ> outrem;
+        // build the filter
+        outrem.setInputCloud(pDownsampledCloud);
+        outrem.setRadiusSearch(f_radius_size);
+        outrem.setMinNeighborsInRadius (2);
+        // apply filter
+        outrem.filter (*pDownsampledCloud);
 }
 
 
 int main(int argc, char **argv) {
     ros::init(argc, argv, "laserScan_to_pointcloud");
     ros::NodeHandle nh;
+
+    nh.param ("ydlidar/leaf_size", m_fLeafSize, 0.1);
+    nh.param ("ydlidar/radius_search", m_fRadius, 0.8);
 
     ros::Subscriber hokuyo_sub;
     hokuyo_sub = nh.subscribe<sensor_msgs::LaserScan>("/scan", 1, ydlidar_callback);
